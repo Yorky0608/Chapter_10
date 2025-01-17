5f669bf5cf0d4b980aa16ba2fe567f8cc7c71689
Caleb York <caleb.york@fwcsstudents.org>
2025-01-17 13:39:22 -0500

    Update Candies.py

--- York_Chapter_10_Practical/Candies.py
+++ York_Chapter_10_Practical/Candies.py
@@ -8,11 +8,6 @@ sheet = wb.active
 max_col = sheet.max_column
 max_row = sheet.max_row
 
-'''The way to write is by take the cell used below and setting cell.value = name/ candy.
-Need a way to keep track where last current cell is and add new cells each new person is added.
-Need to also keep track of adding new candies to the same row/column and a way to separate them to create lists.
-Then wb.save(path) to keep the changes that were made.'''
-
 fav_candies = {}
 
 for a in range(2, max_row + 1):
@@ -22,7 +17,10 @@ for a in range(2, max_row + 1):
     cell1 = cell1.value.title().strip()
     cell2 = cell2.value.lower().strip()
 
-    fav_candies[cell1] = [cell2]
+    for let in cell2:
+        if let == ',':
+            #This is where we need to check for more than one word
+            pass
 
 
 def arrange_by_candies(fav_candies):
@@ -39,20 +37,24 @@ def arrange_by_candies(fav_candies):
     return candy_dict
 
 
-def add_candies(candy, name):
-    fav_candies[name].append(candy)
+def add_candies(cell2, candy):
+    cell2.value = (candy + ', ' + ask_for_candy())
+
 
 
 def ask_for_candy():
     candy = input('What candy would you like to add? ')
     return candy.lower()
 
+count = 0
+def add_new_person(name, count):
+    count += 1
+    new_candy = [[f"Time{count}", name, ask_for_candy()]]
+    for row in new_candy:
+        sheet.append(row)
 
-def add_new_person(name):
-    fav_candies[name] = [ask_for_candy()]
 
-
-def ask_add_candies(fav_candies):
+def ask_add_candies(fav_candies, count, max_row):
     run = True
     while run:
         name = input('What is your name? ')
@@ -67,7 +69,13 @@ def ask_add_candies(fav_candies):
                 try:
                     y_n = input('Would you like to add a candy to your list? y/n ')
                     if y_n == 'y':
-                        add_candies(ask_for_candy(), name)
+                        for index in range(2, max_row + 1):
+                            cell1 = sheet.cell(row=index, column=2)
+                            cell2 = sheet.cell(row=index, column=3)
+
+                            if cell1.value.title().strip() == name:
+                                add_candies(cell2, cell2.value)
+                                break
                     else:
                         break
                 except KeyError:
@@ -75,10 +83,28 @@ def ask_add_candies(fav_candies):
         else:
             print('Your name is not in our database.')
             print('Adding you to our database.')
-            add_new_person(name)
+            add_new_person(name, count)
 
 
-ask_add_candies(fav_candies)
+ask_add_candies(fav_candies, count, max_row)
+
+wb.save(path)
+wb = op.load_workbook(path)
+
+sheet = wb.active
+max_col = sheet.max_column
+max_row = sheet.max_row
+
+fav_candies = {}
+
+for a in range(2, max_row + 1):
+    cell1 = sheet.cell(row=a, column=2)
+    cell2 = sheet.cell(row=a, column=3)
+
+    cell1 = cell1.value.title().strip()
+    cell2 = cell2.value.lower().strip()
+
+    fav_candies[cell1] = [cell2]
 
 print(fav_candies)
 
